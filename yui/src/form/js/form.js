bb15c6b6cd2bbd3eb3257a5902c0487b399c5b88
Alexander Bias <alexander.bias@uni-ulm.de>
2021-02-14 19:53:39 +0100

    Make codechecker fully happy

--- yui/src/form/js/form.js
+++ yui/src/form/js/form.js
@@ -3,7 +3,7 @@
  *
  * @module moodle-availability_cohort-form
  */
-M.availability_cohort = M.availability_cohort || {};
+M.availability_cohort = M.availability_cohort || {}; // eslint-disable-line camelcase
 
 /**
  * @class M.availability_cohort.form
