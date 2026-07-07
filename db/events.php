fcda0215220cf2f30a52788935b265c346ac97b3
Alexander Bias <bias@alexanderbias.de>
2026-07-07 21:27:11 +0200

    Bugfix: Optionally remove orphaned cohort restrictions when a cohort is deleted, so that affected activities and sections do not stay hidden from everyone, resolves #18. (#21)

--- db/events.php
+++ db/events.php
@@ -15,18 +15,18 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Availability Cohort - Version file
+ * Availability cohort - Event observers
  *
- * @package    availability_cohort
- * @copyright  2018 Kathrin Osswald, Ulm University <kathrin.osswald@uni-ulm.de>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package     availability_cohort
+ * @copyright   2026 Alexander Bias <bias@alexanderbias.de>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'availability_cohort';
-$plugin->version = 2025100602;
-$plugin->release = 'v5.1-r2';
-$plugin->requires = 2025100600;
-$plugin->supported = [501, 501];
-$plugin->maturity = MATURITY_STABLE;
+$observers = [
+    [
+        'eventname' => '\core\event\cohort_deleted',
+        'callback' => '\availability_cohort\observer::cohort_deleted',
+    ],
+];
